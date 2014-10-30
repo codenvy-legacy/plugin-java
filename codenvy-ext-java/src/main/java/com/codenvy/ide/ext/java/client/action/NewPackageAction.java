c07a08491896b315ed9cae54e2447118887a9f70
Artem Zatsarynnyy <azatsarynnyy@codenvy.com>
2014-10-30 17:47:51 +0200

    IDEX-1110: update according to api changes

--- codenvy-ext-java/src/main/java/com/codenvy/ide/ext/java/client/action/NewPackageAction.java
+++ codenvy-ext-java/src/main/java/com/codenvy/ide/ext/java/client/action/NewPackageAction.java
@@ -10,32 +10,24 @@
  *******************************************************************************/
 package com.codenvy.ide.ext.java.client.action;
 
-import com.codenvy.api.analytics.logger.AnalyticsEventLogger;
-import com.codenvy.api.project.gwt.client.ProjectServiceClient;
 import com.codenvy.api.project.shared.dto.ItemReference;
 import com.codenvy.ide.api.action.ActionEvent;
-import com.codenvy.ide.api.app.AppContext;
-import com.codenvy.ide.api.editor.EditorAgent;
 import com.codenvy.ide.api.event.NodeChangedEvent;
 import com.codenvy.ide.api.projecttree.AbstractTreeNode;
 import com.codenvy.ide.api.projecttree.generic.StorableNode;
 import com.codenvy.ide.api.selection.Selection;
-import com.codenvy.ide.api.selection.SelectionAgent;
 import com.codenvy.ide.ext.java.client.JavaLocalizationConstant;
 import com.codenvy.ide.ext.java.client.JavaResources;
 import com.codenvy.ide.ext.java.client.JavaUtils;
 import com.codenvy.ide.ext.java.client.projecttree.PackageNode;
 import com.codenvy.ide.ext.java.client.projecttree.SourceFolderNode;
-import com.codenvy.ide.newresource.DefaultNewResourceAction;
+import com.codenvy.ide.newresource.AbstractNewResourceAction;
 import com.codenvy.ide.rest.AsyncRequestCallback;
-import com.codenvy.ide.rest.DtoUnmarshallerFactory;
 import com.codenvy.ide.rest.Unmarshallable;
-import com.codenvy.ide.ui.dialogs.DialogFactory;
 import com.codenvy.ide.ui.dialogs.InputCallback;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import com.google.web.bindery.event.shared.EventBus;
 
 /**
  * Action to create new Java package.
@@ -43,38 +35,16 @@ import com.google.web.bindery.event.shared.EventBus;
  * @author Artem Zatsarynnyy
  */
 @Singleton
-public class NewPackageAction extends DefaultNewResourceAction {
+public class NewPackageAction extends AbstractNewResourceAction {
     private JavaLocalizationConstant localizationConstant;
-    private DtoUnmarshallerFactory   dtoUnmarshallerFactory;
-    private DialogFactory            dialogFactory;
 
     @Inject
-    public NewPackageAction(JavaResources javaResources,
-                            JavaLocalizationConstant localizationConstant,
-                            AppContext appContext,
-                            SelectionAgent selectionAgent,
-                            EditorAgent editorAgent,
-                            ProjectServiceClient projectServiceClient,
-                            DtoUnmarshallerFactory dtoUnmarshallerFactory,
-                            EventBus eventBus,
-                            AnalyticsEventLogger eventLogger,
-                            DtoUnmarshallerFactory unmarshallerFactory,
-                            DialogFactory dialogFactory) {
+    public NewPackageAction(JavaResources javaResources, JavaLocalizationConstant localizationConstant) {
         super(localizationConstant.actionNewPackageTitle(),
               localizationConstant.actionNewPackageDescription(),
               null,
-              javaResources.packageIcon(),
-              appContext,
-              selectionAgent,
-              editorAgent,
-              projectServiceClient,
-              eventBus,
-              eventLogger,
-              unmarshallerFactory,
-              dialogFactory);
+              javaResources.packageIcon());
         this.localizationConstant = localizationConstant;
-        this.dtoUnmarshallerFactory = dtoUnmarshallerFactory;
-        this.dialogFactory = dialogFactory;
     }
 
     @Override
