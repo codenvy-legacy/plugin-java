537e531e41a51f9f1cfae0cf8b531ff24c6cce3f
Vitaly Parfonov <vparfonov@codenvy.com>
2014-12-23 10:31:42 +0200

    new project type

--- codenvy-ext-maven/src/main/java/com/codenvy/ide/extension/maven/server/projecttype/MavenProjectType.java
+++ codenvy-ext-maven/src/main/java/com/codenvy/ide/extension/maven/server/projecttype/MavenProjectType.java
@@ -10,37 +10,27 @@
  *******************************************************************************/
 package com.codenvy.ide.extension.maven.server.projecttype;
 
-import com.codenvy.api.project.server.ProjectTemplateDescriptionLoader;
-import com.codenvy.api.project.server.ProjectTemplateRegistry;
 import com.codenvy.api.project.server.type.ProjectType2;
-import com.codenvy.api.project.shared.dto.ProjectTemplateDescriptor;
 import com.codenvy.ide.ext.java.server.projecttype.JavaProjectType;
 import com.codenvy.ide.extension.maven.shared.MavenAttributes;
-import com.google.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.inject.Inject;
 import javax.inject.Singleton;
-import java.io.IOException;
-import java.util.List;
 
 /** @author Evgen Vidolob */
 @Singleton
 public class MavenProjectType extends ProjectType2 {
     private static final Logger LOG = LoggerFactory.getLogger(MavenProjectType.class);
-    private ProjectTemplateRegistry templateRegistry;
-    private ProjectTemplateDescriptionLoader templateLoader;
 
     @Inject
-    public MavenProjectType(ProjectTemplateRegistry templateRegistry,
-                            ProjectTemplateDescriptionLoader templateLoader,
-                            MavenValueProviderFactory mavenArtifactIdValueProviderFactory,
-                            JavaProjectType javaProjectType) {
+    public MavenProjectType(
+            MavenValueProviderFactory mavenArtifactIdValueProviderFactory,
+            JavaProjectType javaProjectType) {
 
         super(MavenAttributes.MAVEN_ID, MavenAttributes.MAVEN_NAME);
-        this.templateRegistry = templateRegistry;
-        this.templateLoader = templateLoader;
         addVariableDefinition(MavenAttributes.GROUP_ID, "", true, mavenArtifactIdValueProviderFactory);
         addVariableDefinition(MavenAttributes.ARTIFACT_ID, "", true, mavenArtifactIdValueProviderFactory);
         addVariableDefinition(MavenAttributes.VERSION, "", true, mavenArtifactIdValueProviderFactory);
@@ -53,21 +43,5 @@ public class MavenProjectType extends ProjectType2 {
 
         addParent(javaProjectType);
         setDefaultBuilder("maven");
-
-        registerTemplates();
-
-    }
-
-    private void registerTemplates() {
-        try {
-            List<ProjectTemplateDescriptor> list = templateLoader.load(MavenAttributes.MAVEN_ID);
-            for(ProjectTemplateDescriptor templateDescriptor : list) {
-                templateRegistry.register(templateDescriptor);
-            }
-        } catch (IOException e) {
-           LOG.info("MavenProjectType", "Templates not loaded for maven project type");
-        }
-
-
     }
 }
