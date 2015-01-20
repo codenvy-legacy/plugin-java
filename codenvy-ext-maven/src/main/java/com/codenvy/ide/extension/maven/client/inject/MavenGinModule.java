4f04f8c44afb237a377c9d8ac5e5264e135f19c5
Artem Zatsarynnyy <azatsarynnyy@codenvy.com>
2015-01-20 15:42:50 +0200

    IDEX-1855: register Maven wizard

--- codenvy-ext-maven/src/main/java/com/codenvy/ide/extension/maven/client/inject/MavenGinModule.java
+++ codenvy-ext-maven/src/main/java/com/codenvy/ide/extension/maven/client/inject/MavenGinModule.java
@@ -12,10 +12,12 @@ package com.codenvy.ide.extension.maven.client.inject;
 
 import com.codenvy.ide.api.extension.ExtensionGinModule;
 import com.codenvy.ide.api.projecttree.TreeStructureProvider;
+import com.codenvy.ide.api.projecttype.wizard.ProjectWizardRegistrar;
 import com.codenvy.ide.extension.maven.client.build.MavenBuildView;
 import com.codenvy.ide.extension.maven.client.build.MavenBuildViewImpl;
 import com.codenvy.ide.extension.maven.client.projecttree.MavenNodeFactory;
 import com.codenvy.ide.extension.maven.client.projecttree.MavenProjectTreeStructureProvider;
+import com.codenvy.ide.extension.maven.client.wizard.MavenProjectWizardRegistrar;
 import com.google.gwt.inject.client.AbstractGinModule;
 import com.google.gwt.inject.client.assistedinject.GinFactoryModuleBuilder;
 import com.google.gwt.inject.client.multibindings.GinMultibinder;
@@ -34,5 +36,7 @@ public class MavenGinModule extends AbstractGinModule {
 
         install(new GinFactoryModuleBuilder().build(MavenNodeFactory.class));
         GinMultibinder.newSetBinder(binder(), TreeStructureProvider.class).addBinding().to(MavenProjectTreeStructureProvider.class);
+
+        GinMultibinder.newSetBinder(binder(), ProjectWizardRegistrar.class).addBinding().to(MavenProjectWizardRegistrar.class);
     }
 }
---
+++ codenvy-ext-maven/src/main/java/com/codenvy/ide/extension/maven/client/wizard/MavenProjectWizardRegistrar.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2014 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ *******************************************************************************/
+package com.codenvy.ide.extension.maven.client.wizard;
+
+import com.codenvy.ide.api.projecttype.wizard.ProjectWizardRegistrar;
+import com.codenvy.ide.api.wizard.WizardPage;
+import com.codenvy.ide.collections.Array;
+import com.codenvy.ide.collections.Collections;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import static com.codenvy.ide.extension.maven.shared.MavenAttributes.MAVEN_ID;
+
+/**
+ * Provides information for registering Maven project type into project wizard.
+ *
+ * @author Artem Zatsarynnyy
+ */
+public class MavenProjectWizardRegistrar implements ProjectWizardRegistrar {
+    public static final String CATEGORY = "JAVA";
+    private final Array<Provider<? extends WizardPage>> wizardPages;
+
+    @Inject
+    public MavenProjectWizardRegistrar(Provider<MavenPagePresenter> mavenPagePresenter) {
+        wizardPages = Collections.createArray();
+        wizardPages.add(mavenPagePresenter);
+    }
+
+    @Nonnull
+    public String getProjectTypeId() {
+        return MAVEN_ID;
+    }
+
+    @Nullable
+    public String getCategory() {
+        return CATEGORY;
+    }
+
+    @Nonnull
+    public Array<Provider<? extends WizardPage>> getWizardPages() {
+        return wizardPages;
+    }
+}
