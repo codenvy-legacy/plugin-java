104a8eeb48379c530c49aeb129dc6ad1e13e245a
Oleksii Orel <oorel@codenvy.com>
2015-02-24 17:23:33 +0200

    IDEX-1974: Add dialog message if user has no permits for build action

--- codenvy-ext-maven/src/main/java/com/codenvy/ide/extension/maven/client/actions/CustomBuildAction.java
+++ codenvy-ext-maven/src/main/java/com/codenvy/ide/extension/maven/client/actions/CustomBuildAction.java
@@ -12,6 +12,8 @@ package com.codenvy.ide.extension.maven.client.actions;
 
 import com.codenvy.api.analytics.client.logger.AnalyticsEventLogger;
 import com.codenvy.ide.api.action.ActionEvent;
+import com.codenvy.ide.api.action.permits.ActionDenyAccessDialog;
+import com.codenvy.ide.api.action.permits.ActionPermit;
 import com.codenvy.ide.api.action.ProjectAction;
 import com.codenvy.ide.api.app.AppContext;
 import com.codenvy.ide.api.build.BuildContext;
@@ -20,6 +22,7 @@ import com.codenvy.ide.extension.maven.client.MavenResources;
 import com.codenvy.ide.extension.maven.client.build.MavenBuildPresenter;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import com.google.inject.name.Named;
 
 /**
  * Action to build current project.
@@ -29,10 +32,12 @@ import com.google.inject.Singleton;
 @Singleton
 public class CustomBuildAction extends ProjectAction {
 
-    private final AppContext           appContext;
-    private final MavenBuildPresenter  presenter;
-    private final AnalyticsEventLogger eventLogger;
-    private       BuildContext         buildContext;
+    private final AppContext             appContext;
+    private final MavenBuildPresenter    presenter;
+    private final AnalyticsEventLogger   eventLogger;
+    private final ActionPermit           buildActionPermit;
+    private final ActionDenyAccessDialog buildActionDenyAccessDialog;
+    private       BuildContext           buildContext;
 
     @Inject
     public CustomBuildAction(MavenBuildPresenter presenter,
@@ -40,19 +45,27 @@ public class CustomBuildAction extends ProjectAction {
                              MavenLocalizationConstant localizationConstant,
                              AppContext appContext,
                              AnalyticsEventLogger eventLogger,
-                             BuildContext buildContext) {
+                             BuildContext buildContext,
+                             @Named("BuildAction") ActionPermit buildActionPermit,
+                             @Named("BuildAction") ActionDenyAccessDialog buildActionDenyAccessDialog) {
         super(localizationConstant.buildProjectControlTitle(),
               localizationConstant.buildProjectControlDescription(), resources.build());
         this.presenter = presenter;
         this.appContext = appContext;
         this.eventLogger = eventLogger;
         this.buildContext = buildContext;
+        this.buildActionPermit = buildActionPermit;
+        this.buildActionDenyAccessDialog = buildActionDenyAccessDialog;
     }
 
     /** {@inheritDoc} */
     @Override
     public void actionPerformed(ActionEvent e) {
         eventLogger.log(this);
+        if (!buildActionPermit.isAllowed()) {
+            buildActionDenyAccessDialog.show();
+            return;
+        }
         presenter.showDialog();
     }
 
